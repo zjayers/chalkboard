2acfd80fa05772515efdc1183c970cc5d6ffd711
Zach Ayers <zayers.dev@gmail.com>
2020-07-25 12:21:25 -0500

    initial setup for chalkboardjs

---
+++ client/src/components/Cursor/Cursor.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cursor = () => {
+  return <div></div>;
+};
+
+export default Cursor;
---
+++ client/src/components/UserPalette/UserPalette.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserPalette = () => {
+  return <div></div>;
+};
+
+export default UserPalette;
